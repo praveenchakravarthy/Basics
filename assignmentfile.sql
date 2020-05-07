85520ce872cf7666f30f4a7b2c7eb71dd9dad737
praveenchakravarthy <praveenchakravarthy21@gmail.com>
2020-05-07 19:21:08 +0530

    assignment

---
+++ assignmentfile.sql
@@ -0,0 +1,26 @@
+create database assignmentdetail;
+
+use assignmentdetail;
+
+create table assignment1
+
+(
+
+  id int auto_increment primary key,
+
+  Addressline1 varchar(50) null,
+
+  Addressline2 varchar(50) null,
+
+  City varchar(20) null,
+
+  State varchar(20) null,
+
+  Pincode varchar(10) null,
+
+  ContactPerson varchar(20) null,
+
+  ContactNum varchar(10) null,
+
+  );
+
